bf57a1b83280d3bf184960315983c925320b14a6
VAN DAELE <toon.vandaele@inbo.be>
2022-07-08 17:44:11 +0200

    add regio

--- src/1_load_data.R
+++ src/1_load_data.R
@@ -1,23 +1,31 @@
+# Load data
+
+# Dit script maakt verbinding met Watina en laadt alle nodige data in.
+# Het moet enkel worden gerund om een up-to-date data van de Watina
+# databank in te lezen.
+
+# Alle data wordt bewaard in dataframes als .Rds bestand in ./data/interim
+
 # init
 library(tidyverse)
+library(DBI)
 library(watina)
 
-
 watina <- connect_watina()
+
+# Meetpunt locaties
 
 df_mpt <- get_locs(watina) %>%
   collect %>%
   as.data.frame
 
-
 saveRDS(df_mpt, file = "./data/interim/df_mpt.RDS")
 
 
+# Regio's
 
+df_reg <- dbReadTable(watina, "vwDimGebied")
 
-
-
-
-
+saveRDS(df_reg, file = "./data/interim/df_reg.RDS")
 
 dbDisconnect(watina)
